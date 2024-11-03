ab0f5249dd632366a0ba1f04135395702e79ada0
Angelo <barcela2013@gmail.com>
2024-11-03 11:05:29 -0600

    first commit

---
+++ friendly-sniffle-client/src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+
+export default function App() {
+
+    return (
+        <main>
+            <h1 className={'text-5xl text-amber-500'}>coming soon</h1>
+        </main>
+    );
+}
